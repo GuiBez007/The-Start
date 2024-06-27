bf845fceff78dbe517c56cdab8427769aaefa9ba
WillT-007 <guilhermebezerradejesus@gmail.com>
2024-06-27 17:44:22 -0300

    third commit

---
+++ Treinar/VER DPS TMB.py
@@ -0,0 +1,31 @@
+from tkinter import *
+################################################################################
+lista = []
+print("CALCULO DE FORMULA DE BASKARA\n" + "*"*30)
+print("Digite os valores a serem inseridos na formúla\n")
+##esquerda = input("Os à esquerda do sinal de igual(apenas números e separados por espaço:\n")
+##direita = int(input("O à direita do sinal de igual: "))
+##lista = esquerda.split()
+##lista.append(direita)
+##
+##def CalculoBaskara(num_a, num_b, num_c=0):
+##    delta = (num_b*num_b) -4 * num_a * num_c
+##    if delta < 0:
+##        print("DELTA NEGATIVO")
+##        return
+##    else: print("VALOR DELTA:", delta)
+##    
+##    x1 = (-num_b + delta**0.5) / (2 * num_a)
+##    x2 = (-num_b - delta**0.5) / (2 * num_a)
+##    print("Os valores de x1 e x2 são:", round(x1, 2),"e", round(x2, 2))
+##    
+##if len(lista) == 3:
+##    CalculoBaskara(int(lista[0]), int(lista[1]))
+##elif len(lista) == 4:
+##    CalculoBaskara(int(lista[0]), int(lista[1]), int(lista[2]))
+
+janela = Tk()
+janela.title("GUILHERME")
+button(janela, 
+
+janela.mainloop()
